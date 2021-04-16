79876aa007a389beca0b97f46f5aa773bc60143d
Artur Żółkiewski <artur.s.zolkiewski@gmail.com>
2021-04-16 18:31:46 +0200

    Playing around with string and Console.WriteLine()

---
+++ Strings/Strings/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Artur";
+            string lastName = "Żółkiewski";
+            string gretting = "    Hello World     ";
+
+
+
+            Console.WriteLine("Hello " + name + "!");
+            Console.WriteLine($"Hello {name}!");
+
+            Console.WriteLine($"{name} {lastName} this is my full name");
+            Console.WriteLine($"My name has {name.Length} letter");
+            Console.WriteLine($"My last name has {lastName.Length} letter");
+
+
+            Console.WriteLine($"[{gretting}]");
+            string trimmedGretting = gretting.TrimStart();
+            Console.WriteLine($"[{ trimmedGretting}]");
+            trimmedGretting = gretting.TrimEnd();
+            Console.WriteLine($"[{ trimmedGretting}]");
+
+            trimmedGretting = gretting.Trim();
+            Console.WriteLine($"[{ trimmedGretting}]");
+
+            Console.WriteLine($"Upper case greetings without spaces {trimmedGretting.ToUpper()}");
+
+            Console.WriteLine(trimmedGretting.Contains("Hello"));
+            Console.WriteLine(trimmedGretting.Contains("hello"));
+
+            Console.WriteLine(trimmedGretting.StartsWith("hello"));
+            Console.WriteLine(trimmedGretting.StartsWith("Hello"));
+        }
+    }
+}
